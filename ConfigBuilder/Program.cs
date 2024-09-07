d90dabbf5c56ef837b31bb417d0ca3d43cddf88a
Vincent Bouzon <vincent.bouzon@live.fr>
2024-09-07 21:07:46 +0200

    refactor: remove unnecessary subfolder

--- ConfigBuilder/Program.cs
+++ ConfigBuilder/Program.cs
@@ -1,34 +1,16 @@
 ﻿using System.Reflection;
 using System.Text.Json;
 
-var plugins = Directory.GetDirectories("../../../../Plugins");
-var p = "";
-foreach (var plugin in plugins)
-{
-    try
-    {
-        var assemblyConfigurationAttribute = typeof(Program).Assembly.GetCustomAttribute<AssemblyConfigurationAttribute>();
-        var buildConfigurationName = assemblyConfigurationAttribute?.Configuration;
-        var x = Directory.GetDirectories(Path.Combine(plugin, "bin"));
+var solutionFolder = Directory.GetDirectories("../../../../Plugins/");
+var plugin = solutionFolder.Single(e => Path.GetFileName(e) == "BTCPayServer.Plugins.TronUSDT");
+var assemblyConfigurationAttribute = typeof(Program).Assembly.GetCustomAttribute<AssemblyConfigurationAttribute>();
+var buildConfigurationName = assemblyConfigurationAttribute?.Configuration;
         
-        p += $"{Path.GetFullPath(plugin)}/bin/{buildConfigurationName}/net8.0/{Path.GetFileName(plugin)}.dll;";
-        // if (x.Any(s => s.EndsWith("Altcoins-Debug")))
-        // {
-        //     p += $"{Path.GetFullPath(plugin)}/bin/Altcoins-Debug/net8.0/{Path.GetFileName(plugin)}.dll;";
-        // }
-        // else
-        // {
-        // }
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine(e);
-    }
-}
+string[] path = [$"{Path.GetFullPath(plugin)}/bin/{buildConfigurationName}/net8.0/{Path.GetFileName(plugin)}.dll;"];
 
 var content = JsonSerializer.Serialize(new
 {
-    DEBUG_PLUGINS = p
+    DEBUG_PLUGINS = path
 });
 
 Console.WriteLine(content);
