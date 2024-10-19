4e8227b4e643c050063a26fa53283ee2529fe54f
Vincent BOUZON <vincent.bouzon@live.fr>
2024-10-19 13:44:31 +0200

    fix: update to beta19

--- BTCPayServer.Plugins.USDt/Services/Payments/TronUSDtCheckoutModelExtension.cs
+++ BTCPayServer.Plugins.USDt/Services/Payments/TronUSDtCheckoutModelExtension.cs
@@ -13,24 +13,19 @@ public class TronUSDtCheckoutModelExtension(
     private readonly IPaymentLinkExtension _paymentLinkExtension = paymentLinkExtensions.Single(p => p.PaymentMethodId == configurationItem.GetPaymentMethodId());
 
     public PaymentMethodId PaymentMethodId { get; } = configurationItem.GetPaymentMethodId();
-    public string DisplayName => configurationItem.DisplayName;
     public string Image => configurationItem.CryptoImagePath;
     public string Badge => "";
 
     public void ModifyCheckoutModel(CheckoutModelContext context)
     {
-        if (context.Model.Activated)
-        {
+        if (context is not { Handler: TronUSDtLikePaymentMethodHandler handler })
+            return;
+        
+        context.Model.CheckoutBodyComponentName = BitcoinCheckoutModelExtension.CheckoutBodyComponentName;
+        
             context.Model.InvoiceBitcoinUrl = _paymentLinkExtension.GetPaymentLink(context.Prompt, context.UrlHelper);
             context.Model.InvoiceBitcoinUrlQR = context.Model.InvoiceBitcoinUrl;
             context.Model.ShowPayInWalletButton = false;
             context.Model.PaymentMethodCurrency = configurationItem.CurrencyDisplayName;
-        }
-        else
-        {
-            context.Model.InvoiceBitcoinUrl = "";
-            context.Model.InvoiceBitcoinUrlQR = "";
-            context.Model.ShowPayInWalletButton = false;
-        }
     }
 }
