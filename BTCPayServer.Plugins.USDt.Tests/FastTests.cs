528e79f772f0ea4153f3fa52041046bc4aa3f008
b0l0k <vincent.bouzon@live.fr>
2026-04-13 21:52:44 +0200

    test: expand payment flow coverage

--- BTCPayServer.Plugins.USDt.Tests/FastTests.cs
+++ BTCPayServer.Plugins.USDt.Tests/FastTests.cs
@@ -1,6 +1,7 @@
 using BTCPayServer.Plugins.USDt.Services;
 using BTCPayServer.Plugins.USDt.Services.Payments;
 using BTCPayServer.Tests;
+using BTCPayServer.Client.Models;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -81,5 +82,63 @@ public class FastTests : UnitTestBase
         Assert.Equal(
             "ethereum:0xdac17f958d2ee523a2206206994597c13d831ec7@1/transfer?address=0x742d35cc6634c0532925a3b844bc454e4438f44e&uint256=0",
             result);
+    }
+
+    [Fact]
+    public void EvmPaymentLinkReturnsNullWithoutDestination()
+    {
+        Assert.Null(EVMUSDtPaymentLinkExtension.BuildPaymentLink(null, "0xdac17f958d2ee523a2206206994597c13d831ec7", 1, 6, 1m));
+        Assert.Null(EVMUSDtPaymentLinkExtension.BuildPaymentLink(string.Empty, "0xdac17f958d2ee523a2206206994597c13d831ec7", 1, 6, 1m));
+    }
+
+    [Fact]
+    public void EvmPaymentLinkTruncatesFractionalBaseUnits()
+    {
+        var result = EVMUSDtPaymentLinkExtension.BuildPaymentLink(
+            "0x742d35Cc6634C0532925a3b844Bc454e4438f44e",
+            "0xdAC17F958D2ee523A2206206994597C13D831ec7",
+            1,
+            6,
+            0.0000009m);
+
+        Assert.Equal(
+            "ethereum:0xdac17f958d2ee523a2206206994597c13d831ec7@1/transfer?address=0x742d35cc6634c0532925a3b844bc454e4438f44e&uint256=0",
+            result);
+    }
+
+    [Fact]
+    public void UsdtPaymentConfirmedUsesExpectedThresholds()
+    {
+        var paymentData = new USDtPaymentData
+        {
+            TransactionId = "txid",
+            BlockHeight = 1,
+            To = "to",
+            From = "from"
+        };
+
+        paymentData.ConfirmationCount = 1;
+        Assert.False(paymentData.PaymentConfirmed(SpeedPolicy.HighSpeed));
+
+        paymentData.ConfirmationCount = 2;
+        Assert.True(paymentData.PaymentConfirmed(SpeedPolicy.HighSpeed));
+
+        paymentData.ConfirmationCount = 5;
+        Assert.False(paymentData.PaymentConfirmed(SpeedPolicy.MediumSpeed));
+
+        paymentData.ConfirmationCount = 6;
+        Assert.True(paymentData.PaymentConfirmed(SpeedPolicy.MediumSpeed));
+
+        paymentData.ConfirmationCount = 11;
+        Assert.False(paymentData.PaymentConfirmed(SpeedPolicy.LowMediumSpeed));
+
+        paymentData.ConfirmationCount = 12;
+        Assert.True(paymentData.PaymentConfirmed(SpeedPolicy.LowMediumSpeed));
+
+        paymentData.ConfirmationCount = 19;
+        Assert.False(paymentData.PaymentConfirmed(SpeedPolicy.LowSpeed));
+
+        paymentData.ConfirmationCount = 20;
+        Assert.True(paymentData.PaymentConfirmed(SpeedPolicy.LowSpeed));
     }
 }
